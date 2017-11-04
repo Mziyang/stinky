98bf606cd9d82eb890b80805a013ecccd74885fb
Toby <513945442@qq.com>
2017-11-04 21:59:42 +0800

    refined

--- customer/add_to_cart.php
+++ customer/add_to_cart.php
@@ -27,23 +27,21 @@ $customer_id=$row{'id'};
 mysqli_free_result($result);
 //end
 
-if(isset($_GET['pid'])){
+if(isset($_GET['pid'])) {
     $pid = $_GET['pid'];
+    //get product_price start
+    $sql_price = "select unit_price from products where id = $pid";
+    $result_price = mysqli_query($con, $sql_price);
+    $row_price = mysqli_fetch_array($result_price);
+    $price = $row_price['unit_price'];
+    mysqli_free_result($result_price);
+    //end
+
+
+    $sql_insert = "insert into order_details(product_id, temp_cutomer_id, unit_price, quantity) VALUES ($pid,$customer_id,$price,1)";
+    $result_insert = mysqli_query($con, $sql_insert);
+
+    mysqli_close($con);
+
 }
-
-//get product_price start
-$sql_price = "select unit_price from products where id = $pid";
-$result_price = mysqli_query($con,$sql_price);
-$row_price = mysqli_fetch_array($result_price);
-$price = $row_price['unit_price'];
-mysqli_free_result($result_price);
-//end
-
-
-
-
-$sql_insert = "insert into order_details(product_id, temp_cutomer_id, unit_price, quantity) VALUES ($pid,$customer_id,$price,1)";
-$result_insert = mysqli_query($con,$sql_insert);
-
-mysqli_close($con);
 ?>
