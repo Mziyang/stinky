eb8cedfc92befdc7c99ebd02dbd21a4611b977f1
Toby <513945442@qq.com>
2017-11-06 01:01:26 +0800

    update

--- customer/login.php
+++ customer/login.php
@@ -1,5 +1,22 @@
-<title>Login</title>
-<head><link rel="stylesheet" href="../css/main.css"></head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+</head>
+<body>
 <?php
 require_once('../includes/db.php');
 if(!empty($_POST['email'])&&!empty($_POST['password'])) {
@@ -49,8 +66,15 @@ if(!empty($_POST['email'])&&!empty($_POST['password'])) {
         }
 
     }
-    else{
-        echo "Error pwd or email";
+    else{?>
+        <div class="card text-white bg-warning mb-3" style="max-width: 20rem;">
+            <div class="card-header">Warning</div>
+            <div class="card-body">
+                <h4 class="card-title">Error pwd or email</h4>
+                <p class="card-text">Please check your email and password.</p>
+            </div>
+        </div>
+<?php
     }
 
     mysqli_free_result($result);
@@ -74,28 +98,55 @@ if(!empty($_POST['email'])&&!empty($_POST['password'])) {
 
 
 <div class="container">
-    <div class="row text-center">
-        <form action="login.php" method="post" class="text-center">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Please Sign In</h3>
+                </div>
+                <div class="panel-body">
+                    <form role="form" action="login.php" method="post">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="E-mail" name="email" type="email" value="<?php
+                                if(!session_id()){ session_start();}
+                                if(isset($_POST['email'])){echo htmlentities($_POST['email']);}
+                                elseif(isset($_SESSION['register_user'])){echo $_SESSION['register_user'];}//$_SESSION['register_user'] ="";
+                                ?>" required autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password" type="password" minlength="8" maxlength="32" value="" required>
+                            </div>
+<!--                            <div class="checkbox">-->
+<!--                                <label>-->
+<!--                                    <input name="remember" type="checkbox" value="Remember Me">Remember Me-->
+<!--                                </label>-->
+<!--                            </div>-->
+                            <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+            <a href="../admin/admin_login.php" class="btn btn-outline-secondary">Admin?</a>
 
+            <a href="reset_password.php" class="btn btn-outline-warning">Forget Password?</a>
 
-            <p>email:<input name="email" type="email" placeholder="your email address"
-                 value="<?php
-                 if(!session_id()){ session_start();}
-                 if(isset($_POST['email'])){echo htmlentities($_POST['email']);}
-                 elseif(isset($_SESSION['register_user'])){echo $_SESSION['register_user'];}//$_SESSION['register_user'] ="";
-                 ?>" autofocus><br>
-            </p>
+            <a href="register.php" class="btn btn-outline-success">Join Us</a>
 
-            <p>
-                password:<input name="password" type="password" minlength="8" maxlength="32" placeholder="********" ><br>
-            </p>
-            <button type="submit" class="btn">login</button>
-            <a href="../admin/admin_login.php" class="btn">Admin?</a>
-        </form>
-
-        <a href="reset_password.php">Forget Password?</a>
-
-        <a href="register.php">Join Us</a>
+        </div>
     </div>
 
 </div>
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+</body>
+</html>
