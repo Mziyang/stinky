9541f9cc1b9db62bd64d9aac2ef2b2522f1c6b55
Toby <513945442@qq.com>
2017-11-06 23:49:45 +0800

    refined

--- includes/search_bar.php
+++ includes/search_bar.php
@@ -1,15 +1,16 @@
 <!-- search function -->
 <div class="row">
-    <div class="col-sm-8"></div>
-    <div class="col-sm-4">
-        <form class="" method="get" action="<?php if($_SERVER['PHP_SELF'] == "/stinky/index.php"){ echo "includes/";} ?>search.php">
-            <div class="search-bar">
-                <input type="text" name="keyword" placeholder="Product Name Required"
-                       value="<?php if(isset($_GET['keyword'])) { echo $_GET['keyword']; } ?>" required>
+    <div class="col-lg-6"></div>
 
-                <button type="submit" class="btn">Search</button>
+    <div class="col-lg-6">
+        <form class="" method="get" action="<?php if($_SERVER['PHP_SELF'] == "/stinky/index.php"){ echo "includes/";} ?>search.php">
+            <div class="input-group">
+                <input class="form-control" type="text" name="keyword" placeholder="Product Name"
+                       value="<?php if(isset($_GET['keyword'])) { echo $_GET['keyword']; } ?>" required>
+                <span class="input-group-btn">
+                    <button type="submit" class="btn btn-secondary">Search</button>
+                </span>
             </div>
         </form>
     </div>
-
 </div>
